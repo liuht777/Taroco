bf1594cb4c23d120a50734d7c5b5d7468035b93c
liuht <liuhuitao890411>
2018-08-03 14:50:03 +0800

    更新docker配置

--- docker-compose-run.sh
+++ docker-compose-run.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
-echo begin init taroco...
+echo -- -- begin init taroco... -- --
 
 COMPOSE_FILE=./taroco-docs/docker/docker-compose.yml
 JAR_DIR=./taroco-docs/docker/jar
 
-echo stop and remove old docker-compose containers
+echo -- -- stop and remove old docker-compose containers -- --
 if docker-compose -f ${COMPOSE_FILE} ps
     then
         docker-compose -f ${COMPOSE_FILE} stop
         docker-compose -f ${COMPOSE_FILE} rm
 fi
 
-echo build jar
+echo -- -- building jar -- --
 mvn clean package -Dmaven.test.skip=true
 
-echo move jar to ${JAR_DIR}
+echo -- -- move jar to ${JAR_DIR} -- --
 if [ ! -d ${JAR_DIR} ];then
    mkdir -p ${JAR_DIR}
 fi
 
 cp ./taroco-cloud/cloud-registry/target/cloud-registry*.jar ${JAR_DIR}
 cp ./taroco-cloud/cloud-config/target/cloud-config*.jar ${JAR_DIR}
-cp ./cloud-oauth2/authentication-server/target/authentication-server*.jar ${JAR_DIR}
+cp ./taroco-oauth2/authentication-server/target/authentication-server*.jar ${JAR_DIR}
 cp ./taroco-upms-service/target/taroco-upms-service*.jar ${JAR_DIR}
 cp ./taroco-gateway/target/taroco-gateway*.jar ${JAR_DIR}
 
-echo run docker-compose up
+echo -- -- run docker-compose up -- --
 
