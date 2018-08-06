44707b8121d92b96cfc7937d1f9d55f7d2fe3592
liuht <liuhuitao890411>
2018-08-06 17:16:49 +0800

    优化网关 验证码 登录密码加密

--- taroco-gateway/src/main/java/cn/taroco/gateway/filter/MyFilterConstants.java
+++ taroco-gateway/src/main/java/cn/taroco/gateway/filter/MyFilterConstants.java
@@ -20,17 +20,17 @@ public interface MyFilterConstants {
     /**
      * Filter Order for {@link CorsFilter#filterOrder()}
      */
-    int PRE_CORS_LOG_ORDER = 9999;
+    int PRE_CORS_LOG_ORDER = -10000;
 
     /**
      * Filter Order for {@link PreRequestLogFilter#filterOrder()}
      */
-    int PRE_REQUEST_LOG_ORDER = 10000;
+    int PRE_REQUEST_LOG_ORDER = Integer.MAX_VALUE;
 
     /**
      * Filter Order for {@link OptionsTestFilter#filterOrder()}
      */
-    int PRE_OPTIONS_TEST_ORDER = 10001;
+    int PRE_OPTIONS_TEST_ORDER = -10001;
 
     /**
      * Filter Order for {@link WebSocketFilter#filterOrder()}
