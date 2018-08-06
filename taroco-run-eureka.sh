22d30ce60f93c3671c66fc4f8ed3f1afce84cfea
liuht <liuhuitao890411>
2018-08-06 10:04:08 +0800

    优化配置

--- taroco-run-eureka.sh
+++ taroco-run-eureka.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo -- -- begin init taroco... -- --
+echo -- -- begin init taroco eureka... -- --
 
-COMPOSE_FILE=./taroco-docs/docker/docker-compose.yml
+COMPOSE_FILE=./taroco-docs/docker/docker-compose-eureka.yml
 JAR_DIR=./taroco-docs/docker/jar
 
 echo -- -- stop and remove old docker-compose containers -- --
@@ -12,20 +12,19 @@ if docker-compose -f ${COMPOSE_FILE} ps
 fi
 
 echo -- -- building jar -- --
-mvn clean package -Dmaven.test.skip=true
+cd taroco-cloud
+mvn clean package -Dmaven.test.skip=true -pl cloud-registry,cloud-config -am
 
 echo -- -- move jar to ${JAR_DIR} -- --
 if [ ! -d ${JAR_DIR} ];then
    mkdir -p ${JAR_DIR}
 fi
 
-rm -rf ${JAR_DIR}/*
+rm -rf ${JAR_DIR}/cloud-registry*.jar
+rm -rf ${JAR_DIR}/cloud-config*.jar
 
-cp ./taroco-cloud/cloud-registry/target/cloud-registry*.jar ${JAR_DIR}
-cp ./taroco-cloud/cloud-config/target/cloud-config*.jar ${JAR_DIR}
-cp ./taroco-oauth2/authentication-server/target/authentication-server*.jar ${JAR_DIR}
-cp ./taroco-upms/target/taroco-upms*.jar ${JAR_DIR}
-cp ./taroco-gateway/target/taroco-gateway*.jar ${JAR_DIR}
+cp ./cloud-registry/target/cloud-registry*.jar ${JAR_DIR}
+cp ./cloud-config/target/cloud-config*.jar ${JAR_DIR}
 
 echo -- -- run docker-compose up -- --
 docker-compose -f ${COMPOSE_FILE} up -d --build
---
+++ taroco-run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+echo -- -- begin init taroco... -- --
+
+COMPOSE_FILE=./taroco-docs/docker/docker-compose.yml
+JAR_DIR=./taroco-docs/docker/jar
+
+echo -- -- stop and remove old docker-compose containers -- --
+if docker-compose -f ${COMPOSE_FILE} ps
+    then
+        docker-compose -f ${COMPOSE_FILE} stop
+        docker-compose -f ${COMPOSE_FILE} rm --force
+fi
+
+echo -- -- building jar -- --
+mvn clean package -Dmaven.test.skip=true
+
+echo -- -- move jar to ${JAR_DIR} -- --
+if [ ! -d ${JAR_DIR} ];then
+   mkdir -p ${JAR_DIR}
+fi
+
+rm -rf ${JAR_DIR}/authentication-server*.jar
+rm -rf ${JAR_DIR}/taroco-upms*.jar
+rm -rf ${JAR_DIR}/taroco-gateway*.jar
+
+cp ./taroco-oauth2/authentication-server/target/authentication-server*.jar ${JAR_DIR}
+cp ./taroco-upms/target/taroco-upms*.jar ${JAR_DIR}
+cp ./taroco-gateway/target/taroco-gateway*.jar ${JAR_DIR}
+
+echo -- -- run docker-compose up -- --
+docker-compose -f ${COMPOSE_FILE} up -d --build
+
+docker images|grep none|awk '{print $3 }'|xargs docker rmi
