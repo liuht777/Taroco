a1895e06397002cd2566261eb31982d733facc7d
liuht <liuhuitao890411>
2018-08-09 19:12:15 +0800

    upms swagger2

--- taroco-gateway/src/main/java/cn/taroco/gateway/config/SwaggerConfig.java
+++ taroco-gateway/src/main/java/cn/taroco/gateway/config/SwaggerConfig.java
@@ -3,8 +3,10 @@ package cn.taroco.gateway.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
@@ -26,14 +28,18 @@ public class SwaggerConfig {
 
     @Bean
     public Docket createRestApi() {
+        ParameterBuilder tokenPar = new ParameterBuilder();
+        List<Parameter> pars = new ArrayList<>();
+        tokenPar.name("Authorization").description("令牌").modelRef(new ModelRef("string")).parameterType("header")
+                .required(true).build();
+        pars.add(tokenPar.build());
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .securitySchemes(securitySchemes())
-                .securityContexts(securityContexts());
+                .globalOperationParameters(pars);
     }
 
     private ApiInfo apiInfo() {
