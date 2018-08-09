40d128f777229b551ae334c638b95bb272cb8413
liuht <liuhuitao890411>
2018-08-09 17:54:09 +0800

    upms swagger2

---
+++ taroco-gateway/src/main/java/cn/taroco/gateway/config/SwaggerConfig.java
@@ -0,0 +1,60 @@
+package cn.taroco.gateway.config;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * swagger2 配置
+ *
+ * @author liuht
+ * @date 2018/8/9 17:46
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket createRestApi() {
+        ParameterBuilder tokenBuilder = new ParameterBuilder();
+        List<Parameter> parameterList = new ArrayList<>();
+        tokenBuilder.name("Authorization")
+                .defaultValue("去其他请求中获取heard中token参数")
+                .description("令牌")
+                .modelRef(new ModelRef("string"))
+                .parameterType("header")
+                .required(true).build();
+        parameterList.add(tokenBuilder.build());
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                .build()
+                .globalOperationParameters(parameterList);
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Taroco Swagger API ")
+                .description("https://github.com/liuht777/Taroco")
+                .termsOfServiceUrl("https://github.com/liuht777/Taroco")
+                .contact(new Contact("liuht","https://github.com/liuht777","liuht777@qq.com"))
+                .version("1.0")
+                .build();
+    }
+}
