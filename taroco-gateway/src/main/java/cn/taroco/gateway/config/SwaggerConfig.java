43aa37302547aaf24381a27d8e32269436e6117a
liuht <liuhuitao890411>
2018-08-09 18:52:33 +0800

    upms swagger2

--- taroco-gateway/src/main/java/cn/taroco/gateway/config/SwaggerConfig.java
+++ taroco-gateway/src/main/java/cn/taroco/gateway/config/SwaggerConfig.java
@@ -3,14 +3,11 @@ package cn.taroco.gateway.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.schema.ModelRef;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.service.Parameter;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -29,21 +26,14 @@ public class SwaggerConfig {
 
     @Bean
     public Docket createRestApi() {
-        ParameterBuilder tokenBuilder = new ParameterBuilder();
-        List<Parameter> parameterList = new ArrayList<>();
-        tokenBuilder.name("Authorization")
-                .description("access_token")
-                .modelRef(new ModelRef("string"))
-                .parameterType("header")
-                .required(true).build();
-        parameterList.add(tokenBuilder.build());
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .globalOperationParameters(parameterList);
+                .securitySchemes(securitySchemes())
+                .securityContexts(securityContexts());
     }
 
     private ApiInfo apiInfo() {
@@ -51,8 +41,34 @@ public class SwaggerConfig {
                 .title("Taroco Swagger API ")
                 .description("https://github.com/liuht777/Taroco")
                 .termsOfServiceUrl("https://github.com/liuht777/Taroco")
-                .contact(new Contact("liuht","https://github.com/liuht777","liuht777@qq.com"))
+                .contact(new Contact("liuht", "https://github.com/liuht777", "liuht777@qq.com"))
                 .version("1.0")
                 .build();
+    }
+
+    private List<SecurityContext> securityContexts() {
+        List<SecurityContext> contexts = new ArrayList<>(1);
+        SecurityContext securityContext = SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex("^(?!auth).*$"))
+                .build();
+        contexts.add(securityContext);
+        return contexts;
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        List<SecurityReference> references = new ArrayList<>(1);
+        references.add(new SecurityReference("Authorization", authorizationScopes));
+        return references;
+    }
+
+    private List<ApiKey> securitySchemes() {
+        List<ApiKey> apiKeys = new ArrayList<>(1);
+        ApiKey apiKey = new ApiKey("Authorization", "Authorization", "header");
+        apiKeys.add(apiKey);
+        return apiKeys;
     }
 }
