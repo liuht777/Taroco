80eba88504f9eece081474ee316ffe086e5952ca
liuht <liuhuitao890411>
2018-08-13 16:23:28 +0800

    增加monitor

--- taroco-run.sh
+++ taroco-run.sh
@@ -22,11 +22,13 @@ fi
 rm -rf ${JAR_DIR}/authentication-server*.jar
 rm -rf ${JAR_DIR}/taroco-upms*.jar
 rm -rf ${JAR_DIR}/cloud-admin*.jar
+rm -rf ${JAR_DIR}/cloud-monitor*.jar
 rm -rf ${JAR_DIR}/taroco-gateway*.jar
 
 cp ./taroco-oauth2/authentication-server/target/authentication-server*.jar ${JAR_DIR}
 cp ./taroco-upms/target/taroco-upms*.jar ${JAR_DIR}
 cp ./taroco-cloud/cloud-admin/target/cloud-admin*.jar ${JAR_DIR}
+cp ./taroco-cloud/cloud-monitor/target/cloud-monitor*.jar ${JAR_DIR}
 cp ./taroco-gateway/target/taroco-gateway*.jar ${JAR_DIR}
 
 echo -- -- run docker-compose up -- --
